1f739f1894ece1f67940765f13e53b8086024414
Kobus Herbst <kobush@gmail.com>
2021-01-05 10:03:08 +0200

    Final IndividualMemberships run

--- src/test2.jl
+++ src/test2.jl
@@ -2,6 +2,6 @@ using Dates
 using Arrow
 using Dates
 using CSV
-memberships = Arrow.Table(joinpath("D:\\Data\\SAPRIN_Data", "Agincourt", "Staging", "IndividualMemberships.arrow")) |> DataFrame
+memberships = Arrow.Table(joinpath("D:\\Data\\SAPRIN_Data", "AHRI", "Staging", "IndividualMemberships.arrow")) |> DataFrame
 # "D:\\Data\\SAPRIN_Data"
 # m = filter([:IndividualId] => id -> id >= 1 && id <= 10, memberships)
