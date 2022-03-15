25e3d68981357dc25611ab5f5ac0bcd72d85d61a
Kobus Herbst <kobusherbst@users.noreply.github.com>
2022-03-15 08:38:37 +0200

    Added converting education status data to Stata

--- src/labourstatus.jl
+++ src/labourstatus.jl
@@ -8,7 +8,14 @@ function convertlabourtostata(node::String)
     runstata("labour.do",settings.Version, node, joinpath(stagingpath(node),"LabourStatusRaw"))
 end
 
+function converteducationtostata(node::String)
+    arrowtostatar(node,stagingpath(node),"EducationStatuses","EducationStatusRaw")
+#    runstata("labour.do",settings.Version, node, joinpath(stagingpath(node),"EducationStatusRaw"))
+end
 
-convertlabourtostata("AGINCOURT")
-convertlabourtostata("AHRI")
-convertlabourtostata("DIMAMO")
+# convertlabourtostata("AGINCOURT")
+# convertlabourtostata("AHRI")
+# convertlabourtostata("DIMAMO")
+
+converteducationtostata("AHRI")
+converteducationtostata("DIMAMO")
